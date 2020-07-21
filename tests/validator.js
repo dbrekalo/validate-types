d6c567b669dcc3eb1f159817b91fafc2f0ef5ef4
Damir Brekalo <dbrekalo@gmail.com>
2020-07-21 16:07:44 +0200

    Add params to validator check

--- tests/validator.js
+++ tests/validator.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'validator',
     validate: function(params) {
-        return Boolean(params.testConfig(params.fieldValue));
+        return Boolean(params.testConfig(params.fieldValue, params));
     },
     message: function(params) {
         return 'Field "' + params.fieldName + '" failed validation';
