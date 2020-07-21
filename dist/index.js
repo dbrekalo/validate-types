8205ac6395a7151b7a00d19c8e7fcc68566fd0c5
Damir Brekalo <dbrekalo@gmail.com>
2020-07-21 16:09:34 +0200

    Update distribution files

--- dist/index.js
+++ dist/index.js
@@ -107,7 +107,7 @@
     var validator = {
         name: 'validator',
         validate: function(params) {
-            return Boolean(params.testConfig(params.fieldValue));
+            return Boolean(params.testConfig(params.fieldValue, params));
         },
         message: function(params) {
             return 'Field "' + params.fieldName + '" failed validation';
